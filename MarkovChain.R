c8aa5294f9f9f51c5beb9b240190421839d9cb99
K. Hazel Kwon <k.hazel.kwon@gmail.com>
2016-08-19 11:38:34 -0700

    Update MarkovChain.R

--- MarkovChain.R
+++ MarkovChain.R
@@ -1,28 +1,32 @@
-dat<-data.frame(replicate(20,sample(c(1, 2, 3), size = 100, replace=TRUE)))
+#The csv datafile including each row as a sequence of events
+#Intend to comput Steady state distribution for each sequence.
+#Therefore, I loop the process for each row (j = 1 through 100) 
 
-#coerce data frame as matrix
-DAT <- as.matrix(dat)
-DAT
+DAT <- as.matrix(read.csv("", header = T))
 
-#subset the first row from the matrix DAT
-x <- DAT[1,]
-x
+for(j in 1:100)
+{
+  x <- DAT[j,]
 
-#compute transition probability matrix
-p <- matrix(nrow = 3, ncol = 3, 0)
-for (t in 1:(length(x) - 1)) p[x[t], x[t + 1]] <- p[x[t], x[t + 1]] + 1
-for (i in 1:3) p[i, ] <- p[i, ] / sum(p[i, ])
-p
+  #compute transition probability matrix
+  p <- matrix(nrow = 3, ncol = 3, 0) 
+  for (t in 1:(length(x) - 1)) p[x[t], x[t + 1]] <- p[x[t], x[t + 1]] + 1 
+  for (i in 1:3) p[i, ] <- p[i, ] / sum(p[i, ])
+  p[is.nan(p)] <- 0
+  
+  #compute steady state distribution
+  n = ncol(p)
+  A = t(p - diag(n))
+  A = rbind(A, rep(1, n))
+  b = c(rep(0, n), 1)
+  steadystate = qr.solve(A, b)
+  steadystate 
+  SS[j, ] <- steadystate 
+  sum(steadystate[1], steadystate[2], steadystate[3])
+}
 
-#compute steady state distribution
-n = ncol(p)
-A = t(p - diag(n))
-A = rbind(A, rep(1, n))
-b = c(rep(0, n), 1)
+##Final Product
+write.csv(SS,"directory/filename.csv" )
 
-A
-b
 
-steadystate = qr.solve(A, b)
-steadystate
 
